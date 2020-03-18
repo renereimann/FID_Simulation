bb9326278b37609a67fe55bdc4eee75d95b40ceb
Rene Reimann <reimann@physik.rwth-aachen.de>
2020-03-18 17:17:32 +0100

    Implementation of Biot-Savart Law and its integal to calculate B-field of coil
    * We implemented the Biot-Savart Law for random positions.
    * We implement the path along the coil as a helix
    * The integration is done using scipy.integral.quad
    * relevant parameters are coil diameter, radius and number of turns
    * For verification we calculate and plot the B_z component of the field
      alon the z axis.
    * For comparison we compare the plot with the plot from DocDB 16856
      Slide 5 (used parameters for coil dimensions on slide 3)
      * The plots differ, however the plot from DocDB can be reproduced if
        the double diameter is used. This indicates a problem with radius vs
        diameter in either my code or the code used in DocDB 16856.
    * Used WebPlotDigitizer to extract the diameter of the coil from the
      lower figure of Slide 3. The coil diameter is of the order of 5.5 mm
      and thus inconsistent with the indicated 2.3 mm in the upper plot.
      Due to that inconsistency we can for now assume that the 2.3 mm in the
      upper plot should indicate the radius instead of the diameter, which
      would than lead to consistent results.
    
    --> We can trust the implementation of the Biot-Savart Law, its
    integration along a coil path.
    --> We have to figure out why we need a factor of 2 in the diameter
    definition, but it is likely that the problem can also be in the DocDB
    presentation.
    
    Note: To run the script add a `plots` directory. We do not push the plot
    here for the moment.

--- FID_simulation.py
+++ FID_simulation.py
@@ -2,15 +2,21 @@
 # https://gm2-docdb.fnal.gov/cgi-bin/private/RetrieveFile?docid=16856&filename=fid_simulation.pdf&version=4
 
 import numpy as np
+from scipy import integrate
 import matplotlib.pyplot as plt
+
 # Constants go here
+mu0 = 1.256637062e-6 # N/A^2
 
 # Geometry
 probe_total_length = 100.00 #mm
 probe_total_diameter = 8.00 #mm
+
+# Fixed Probe
 coil_diameter = 2.3 #mm
 coil_length = 15.0 #mm
 coil_turns = 30
+
 cell_type = "cylinder"
 cell_length = 30.0 #mm
 cell_diameter = 1.5 #mm
@@ -18,12 +24,13 @@ cell_diameter = 1.5 #mm
 # material
 probe_material = "Petroleum Jelly"
 T2 = 40*1e-3 # sec # how mature are these values
-gamma =  # gyromagnetic ratio of the spin sample particles.
+#gamma =  # gyromagnetic ratio of the spin sample particles.
 
 # magnetic field
 B0 = 1.45 # T, static field strength
-B1 = # RF field strength
+# B1 = # RF field strength
 f_NMR = 61.79 # MHz
+I = 5 #A
 
 # simulation
 seed = 12345
@@ -65,12 +72,49 @@ spin_phi = np.random.uniform(0, 2*np.pi, size=N_particles)
 #      - closed loop
 #      - constant current, can factor out the I from integral
 
+coil_path_parameter = np.linspace(0, 2*np.pi*coil_turns, 1000)
+
+lx = lambda phi: coil_diameter/2. * np.sin(phi)
+ly = lambda phi: coil_diameter/2. * np.cos(phi)
+lz = lambda phi: coil_length * phi / (2*np.pi*coil_turns) - coil_length/2
+
+dlx = lambda phi: coil_diameter/2. * np.cos(phi)
+dly = lambda phi: -coil_diameter/2. * np.sin(phi)
+dlz = lambda phi: coil_length / (2*np.pi*coil_turns)
+
+dist = lambda phi, x, y, z: np.sqrt((lx(phi)-x)**2+(ly(phi)-y)**2+(lz(phi)-z)**2)
+integrand_x = lambda phi, x, y, z: ( dly(phi) * (z-lz(phi)) - dlz(phi) * (y-ly(phi)) ) / dist(phi, x, y, z)**3
+integrand_y = lambda phi, x, y, z: ( dlz(phi) * (x-lx(phi)) - dlx(phi) * (z-lz(phi)) ) / dist(phi, x, y, z)**3
+integrand_z = lambda phi, x, y, z: ( dlx(phi) * (y-ly(phi)) - dly(phi) * (x-lx(phi)) ) / dist(phi, x, y, z)**3
+
+B1_x = lambda x, y, z : mu0/(4*np.pi) * I * integrate.quad(lambda phi: integrand_x(phi, x, y, z), 0, 2*np.pi*coil_turns)[0]
+B1_y = lambda x, y, z : mu0/(4*np.pi) * I * integrate.quad(lambda phi: integrand_y(phi, x, y, z), 0, 2*np.pi*coil_turns)[0]
+B1_z = lambda x, y, z : mu0/(4*np.pi) * I * integrate.quad(lambda phi: integrand_z(phi, x, y, z), 0, 2*np.pi*coil_turns)[0]
+
+if True:
+    # make a plot for comparison
+    cross_check = np.genfromtxt("./RF_coil_field_cross_check.txt", delimiter=", ")
+    zs = np.linspace(-15, 15, 1000)
+    plt.figure()
+    plt.plot(cross_check[:,0], cross_check[:,1], label="Cross-Check from DocDB 16856, Slide 5\n$\O=2.3\,\mathrm{mm}$, $L=15\,\mathrm{mm}$, turns=30", color="orange")
+    B1_z0 = B1_z(0, 0, 0)
+    plt.plot(zs, [B1_z(0, 0, z)/B1_z0 for z in zs], label="My calculation\n$\O=2.3\,\mathrm{mm}$, $L=15\,\mathrm{mm}$, turns=30", color="k")
+    coil_diameter = 2*2.3 # mm
+    B1_z0 = B1_z(0, 0, 0)
+    plt.plot(zs, [B1_z(0, 0, z)/B1_z0 for z in zs], label="My calculation\n$\O=4.6\,\mathrm{mm}$, $L=15\,\mathrm{mm}$, turns=30", color="k", ls=":")
+    plt.xlabel("z / mm")
+    plt.ylabel("$B_z(0,0,z)\, /\, B_z(0,0,0)$")
+    plt.legend(loc="lower right")
+    plt.title("Magnetic field of the coil (static)")
+    plt.tight_layout()
+    plt.savefig("./plots/coil_field_distribution.pdf", bbox_inches="tight")
+
 ########################################################################################
 
 # apply RF field
 # B1 field strength of RF field
 # for time of t_pi2 = pi/(2 gamma B1)
-
+'''
 t_90 = np.pi/(2*gamma*B1)
 
 # spin
@@ -99,3 +143,4 @@ phi(t) = np.sum(coil_turns*vec(B2(vec(r_mu)))*vec(mu(t)))/I1
 ########################################################################################
 
 # integrate dM/dt with RK4
+'''
