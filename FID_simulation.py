622b2bc48bf5853ac9dab43f8fe60fdde43d5afc
Rene Reimann <reimann@physik.rwth-aachen.de>
2020-03-18 20:06:27 +0100

    Implemented definition of magnetization of cells and defined all physics constants
    With this commit we add
    * A header comment explaining the purpose of the script, the author
    * Added reference to second DocDB document that we are using to set up
      the script
    * Definition of physical constants, rechecked several of them
    * Units are currently given as comments, maybe we better use a software
      package for them
    * The geometry is now the geometry of the fixed probes
    * Added constants and properties of petroleum jelly, wavlues from
      wolframalpha.com
    * Added values needed to calculat magnetization, (e.g. Temperature)
    * Temperature is for now 300 K, which corresponds to warm room
      temperature
    * Use the same coil current as in DocDB 11289, which is 0.8 A
    * Renamed N_particles to N_cells because approach does not track
      individual particles but very small cells containing a large number of
      particles.
    * Added formulas to calculate magnitization of cell. This depends on
      material properties, Volume of sample cell, temperature and other
      constants.
    * Cross-checked some units of calculated values and if order of
      magnitude makes sens to me. No indepth test yet.
    --> We are now ready to use the magnitization of the cells in further
    steps of the simulation.

--- FID_simulation.py
+++ FID_simulation.py
@@ -1,59 +1,80 @@
-# Based on DocDB # 16856
+# This script simulates the FID signal of a pNMR probe.
+#
+# Author: René Reimann (2020)
+#
+# The ideas are based on DocDB #16856 and DocDB #11289
 # https://gm2-docdb.fnal.gov/cgi-bin/private/RetrieveFile?docid=16856&filename=fid_simulation.pdf&version=4
+# https://gm2-docdb.fnal.gov/cgi-bin/private/RetrieveFile?docid=11289&filename=FIDSimulaitonNote.pdf&version=1
+
+########################################################################################
+# Import first
 
 import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
 
-# Constants go here
-mu0 = 1.256637062e-6 # N/A^2
+########################################################################################
+# Definition of constants used within the script
 
-# Geometry
+# Universal Constants
+mu0 = 4*np.pi*1e-7 # N/A^2 # magnetic constant, vacuum permeability, permeability of free space, permeability of vacuum, magnetic permeability
+kB = 1.380649e-23 # J/K  # Boltzmann constant
+N_A = 6.02214076e23 # 1/mol # Avogadro constant
+mu_p = 1.41060679736e-26 # J/T # proton magnetic moment
+gamma_p = 2.6752218744e8 # 1/s/T # gyromagnetic ratio of proton (the spin sample particles)
+
+# Geometry - Fixed Probe
 probe_total_length = 100.00 #mm
 probe_total_diameter = 8.00 #mm
-
-# Fixed Probe
 coil_diameter = 2.3 #mm
 coil_length = 15.0 #mm
 coil_turns = 30
-
-cell_type = "cylinder"
 cell_length = 30.0 #mm
 cell_diameter = 1.5 #mm
 
-# material
-probe_material = "Petroleum Jelly"
-T2 = 40*1e-3 # sec # how mature are these values
-#gamma =  # gyromagnetic ratio of the spin sample particles.
+# Probe Material
+# values from wolframalpha.com "petrolium jelly"
+probe_material_name = "Petroleum Jelly"
+probe_material_formula = "C40H46N4O10"
+molar_mass = 742.8 # g/mol
+density = 0.848 # g/cm^3
+T2 = 40*1e-3     # sec
 
 # magnetic field
-B0 = 1.45 # T, static field strength
-# B1 = # RF field strength
-f_NMR = 61.79 # MHz
-I = 5 #A
+B0 = 1.45            # T, static field strength
+T = 273.15 + 26.85   # K
+I = 0.8 # A
+# B1, RF field strength, will be calculated below
+# f_NMR = 61.79 # MHz, circuit of the probe tuned for this value
+# impedance = 50 # Ohm
 
 # simulation
 seed = 12345
-N_particles = 1000
+N_cells = 1000
 
 ########################################################################################
 # that about motion / diffusion within material
 # what about other components of the probe
 # what about spin-spin interactions
 
-
 # setup random number
 rng = np.random.RandomState(seed)
 
-# setup particles and spins
-r = np.sqrt(np.random.uniform(0,cell_diameter**2/4, size=N_particles))
-phi = np.random.uniform(0, 2*np.pi, size=N_particles)
-z = np.random.uniform(-cell_length/2., cell_length/2., size=N_particles)
+# setup cells and magnetization
+r = np.sqrt(np.random.uniform(0,cell_diameter**2/4, size=N_cells))
+phi = np.random.uniform(0, 2*np.pi, size=N_cells)
+z = np.random.uniform(-cell_length/2., cell_length/2., size=N_cells)
 x = r*np.sin(phi)
 y = r*np.cos(phi)
 
-spin_cosTheta = np.random.uniform(-1, 1, size=N_particles)
-spin_phi = np.random.uniform(0, 2*np.pi, size=N_particles)
+nuclear_polarization = (np.exp(mu_p*B0/kB/T) - np.exp(-mu_p*B0/kB/T))/(np.exp(mu_p*B0/kB/T) + np.exp(-mu_p*B0/kB/T))
+V_cell = np.pi*(cell_diameter/2.)**2 * cell_length
+number_density = density/molar_mass*N_A  # 1/cm^3
+M = mu_p * number_density * nuclear_polarization
+dipole_moment = M * V_cell/N_cells # J/T
+
+spin_cosTheta = np.random.uniform(-1, 1, size=N_cells)
+spin_phi = np.random.uniform(0, 2*np.pi, size=N_cells)
 
 ########################################################################################
 
@@ -72,6 +93,7 @@ spin_phi = np.random.uniform(0, 2*np.pi, size=N_particles)
 #      - closed loop
 #      - constant current, can factor out the I from integral
 
+# coil path is implemented as perfect helix
 coil_path_parameter = np.linspace(0, 2*np.pi*coil_turns, 1000)
 
 lx = lambda phi: coil_diameter/2. * np.sin(phi)
