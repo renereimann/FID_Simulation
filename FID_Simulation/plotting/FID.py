b7aab10b26bbe3ecf507d8aa05dd2fa4081c59df
Rene Reimann <reimann@physik.rwth-aachen.de>
2020-08-11 17:28:18 +0200

    stream lined FID plotting

--- FID_Simulation/plotting/FID.py
+++ FID_Simulation/plotting/FID.py
@@ -7,88 +7,40 @@ from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm as mcolors
 
-from units import *
+from ..units import us, ms, uV
 
-def plot_RF_pulse_3D(data, savepath=None):
-    fig = plt.figure(figsize=(16,12))
+def plot_FID(times, fluxes, labels=None, inset=[[1.8, 2.8], [-1, 1]], savepath=None, close_on_exit=False):
+    times = np.atleast_2d(times)
+    fluxes = np.atleast_2d(fluxes)
+    if labels is not None:
+        labels = np.atleast_2d(labels)
+    else:
+        labels = [None]*len(fluxes)
 
-    ax = fig.add_subplot(221)
-    ax.set_title(r"$x \approx y \approx z \approx 0$")
-    ax.plot(data[:,0]/us, data[:,4], label="$M_x$")
-    ax.plot(data[:,0]/us, data[:,6], label="$M_z$")
-    ax.plot(data[:,0]/us, data[:,5], label="$M_y$", color="k")
-    ax.legend(loc=3)
-    ax.grid()
-    ax.set_xlim(0,14)
-    ax.set_ylim(-1,1)
-    ax.set_xlabel("time / $\mu$s")
-    ax.set_ylabel(r"$M(\sim0,\sim0,\sim0)$")
-    axins = inset_axes(ax, width="50%",  height="30%", loc=1)
-    axins.plot(data[:,0]/us, data[:,4])
-    axins.plot(data[:,0]/us, data[:,6])
-    axins.plot(data[:,0]/us, data[:,5], color="k")
-    axins.set_xlim(0.5,0.7)
-    axins.set_ylim(-0.25,0.25)
-    axins.yaxis.get_major_locator().set_params(nbins=7)
-    axins.xaxis.get_major_locator().set_params(nbins=7)
-    plt.yticks(visible=False)
-    #mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
+    fig, ax = plt.subplots()
+    for time, flux, label in zip(times, fluxes, labels):
+        ax.plot(time/ms, flux/uV, label=label)
+    ax.set_xlabel("t / ms")
+    ax.set_xlim([0, 10])
+    ax.set_ylabel("induced voltage in coil / $\mu$V")
+    if any([l is not None for l in labels]):
+        ax.legend()
+    plt.tight_layout()
 
-    ax = fig.add_subplot(222)
-    ax.set_title("averaged over volume")
-    ax.plot(data[:,0]/us, data[:,1], label="$M_x$")
-    ax.plot(data[:,0]/us, data[:,3], label="$M_z$")
-    ax.plot(data[:,0]/us, data[:,2], label="$M_y$", color="k")
-    ax.legend(loc=3)
-    ax.grid()
-    ax.set_xlim(0,14)
-    ax.set_ylim(-1,1)
-    ax.set_xlabel("time / $\mu$s")
-    ax.set_ylabel(r"$\langle M_i \rangle$")
-    axins = inset_axes(ax, width="50%",  height="30%", loc=1)
-    axins.plot(data[:,0]/us, data[:,1])
-    axins.plot(data[:,0]/us, data[:,3])
-    axins.plot(data[:,0]/us, data[:,2], color="k")
-    axins.set_xlim(0.5,0.7)
-    axins.set_ylim(-0.25,0.25)
-    axins.yaxis.get_major_locator().set_params(nbins=7)
-    axins.xaxis.get_major_locator().set_params(nbins=7)
-    plt.yticks(visible=False)
-    #mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
-
-    step = -10
-    ax = fig.add_subplot(223, projection='3d')
-    sc = ax.scatter(data[::step,4], data[::step,6], data[::step,5], c=data[::step,0]/us,
-                    marker="o", cmap="jet", vmin=data[0,0]/us, vmax=data[-1,0]/us)
-    ax.set_xlim(-1,1)
-    ax.set_ylim(-1,1)
-    ax.set_zlim(-1,1)
-    ax.set_xlabel("Mx")
-    ax.set_ylabel("Mz")
-    ax.set_zlabel("My")
-
-    ax.xaxis.get_major_locator().set_params(nbins=5)
-    ax.yaxis.get_major_locator().set_params(nbins=5)
-    ax.zaxis.get_major_locator().set_params(nbins=5)
-    plt.colorbar(sc, ax=ax).set_label("time / $\mu$s")
-
-
-    ax = fig.add_subplot(224, projection='3d')
-    sc = ax.scatter(data[::step,1], data[::step,3], data[::step,2], c=data[::step,0]/us,
-                    #sc = ax.scatter(data[:,1], data[:,3], -data[:,2], c=data[::-1,0]/us,
-                    marker="o", cmap="jet", vmin=data[0,0]/us, vmax=data[-1,0]/us)
-    ax.set_xlim(-1,1)
-    ax.set_ylim(-1,1)
-    ax.set_zlim(-1,1)
-    ax.set_xlabel("Mx")
-    ax.set_ylabel("Mz")
-    ax.set_zlabel("My")
-
-    ax.xaxis.get_major_locator().set_params(nbins=5)
-    ax.yaxis.get_major_locator().set_params(nbins=5)
-    ax.zaxis.get_major_locator().set_params(nbins=5)
-    plt.colorbar(sc, ax=ax).set_label("time / $\mu$s")
+    if inset is not None:
+        axins = inset_axes(ax, width="60%",  height="30%", loc=1)
+        for time, flux in zip(times, fluxes):
+            axins.plot(time/ms, flux/uV)
+        axins.set_xlim(inset[0])
+        axins.set_ylim(inset[1])
+        axins.yaxis.get_major_locator().set_params(nbins=7)
+        axins.xaxis.get_major_locator().set_params(nbins=7)
+        plt.xticks(visible=False)
+        plt.yticks(visible=False)
+        mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
 
     if savepath is not None:
         fig.savefig(savepath.replace("png", "pdf"), bbox_inches="tight")
         fig.savefig(savepath.replace("pdf", "png"), dpi=200)
+    if close_on_exit:
+        plt.close()
