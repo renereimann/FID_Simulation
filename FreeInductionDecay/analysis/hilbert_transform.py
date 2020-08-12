d13c83b4d2ba7c9d8b52ed9c4f12f07aa9d6d6c8
Rene Reimann <reimann@physik.rwth-aachen.de>
2020-08-12 17:12:32 +0200

    made the hilbert transform a class that can give the phase function, envelope function, real and imaginary part etc.

--- FreeInductionDecay/analysis/hilbert_transform.py
+++ FreeInductionDecay/analysis/hilbert_transform.py
@@ -1,20 +1,47 @@
+import numpy as np
+from scipy.signal import hilbert
+from ..units import uV, ms
 
+class HilbertTransform(object):
+    def __init__(self, times, flux):
+        if len(times) != len(flux):
+            raise AttributeError("times and flux must have the same dimension")
+        self.N = len(flux)
+        self.time = times / ms
+        self.flux = flux / uV
+        self.h = hilbert(self.flux)
 
+    def real(self):
+        return np.real(self.h)
 
+    def imag(self):
+        return np.imag(self.h)
+
+    def PhaseFunction(self):
+        phi = np.arctan2(self.imag(), self.real())
+        phi = np.arctan(self.imag()/self.real())
+        oscillations = np.cumsum(np.pi*(np.sign(self.flux[1:]) != np.sign(self.flux[:-1])))
+        phi += np.concatenate([[0], oscillations])
+        return self.time, phi
+
+    def EnvelopeFunction(self):
+        return self.time, np.sqrt(self.imag()**2 + self.real()**2)
+
+    def plot_phase_function(self, fig=None):
+        if fig is None:
+            fig, ax = plt.subplots()
+
+        t, phi = self.PhaseFunction()
+        ax.scatter(t, np.degrees(phi))
+
+"""
 def get_phase_from_hilbert(flux, window=None):
-    N = len(flux)
-    flux /= uV
-    if window is not None:
-        flux *= window(N)
-    h = hilbert(flux)
-    phi = np.arctan(np.imag(h)/np.real(h))
-    oscillations = np.cumsum(np.pi*(np.sign(flux[1:]) != np.sign(flux[:-1])))
-    phi += np.concatenate([[0], oscillations])
 
     plt.figure()
-    plt.scatter(times/ms, np.degrees(phi- 314.16700919*times/ms)%180)
+    plt.scatter(self.time, np.degrees(self.- 314.16700919*times/ms)%180)
     plt.axhline(np.degrees(-1.57126946)%180, ls="--", color="k")
     plt.xlim([0.4, 0.6])
     plt.ylim(88, 92)
 
 get_phase_from_hilbert(flux, window=signal.hamming)
+"""
