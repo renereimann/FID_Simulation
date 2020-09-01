e1c26ed2ceb93dee8e4b32c3bb6bf41566aa25e2
Rene Reimann <reimann@physik.rwth-aachen.de>
2020-09-01 20:15:29 +0200

    Added functions to run the fit of the phase with optimized fit range.

--- FreeInductionDecay/analysis/phase_fit.py
+++ FreeInductionDecay/analysis/phase_fit.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.optimize import minimize
 from scipy.ndimage.filters import uniform_filter1d
 from ..units import *
+from .hilbert_transform import HilbertTransform
+import matplotlib.pyplot as plt
 
 # optimal window size is 2pi/omega_bar
 def running_mean(time, val, window_size):
@@ -55,3 +57,62 @@ def poly_fit(time, phi,time_range=[0,2], x0=None, tol=1e-4):
         x0 = np.random.normal(size=5)
     res = minimize(chi2, x0, tol=tol)
     return res
+
+def phase_analysis(time, flux, t0, t_range, verbose=False, plotting=False, smoothing=True, tol=None, x0=None, window_size=0.05*ms):
+    hilbert = HilbertTransform(time, flux)
+    _, env = hilbert.EnvelopeFunction()
+    _, phi = hilbert.PhaseFunction()
+
+    phi_raw = phi[:]
+    if smoothing:
+        _, phi = running_mean(time,  phi, window_size=window_size)
+
+    width = t_range[1]-t_range[0]
+    mask = np.logical_and(time > t_range[0], time < t_range[1])
+    res = poly_fit((time-t0)/width, phi, time_range=(t_range-t0)/width, x0=x0, tol=tol)
+    phi_fit = fit_func((time[mask]-t0)/width, res.x)
+    frequency = res.x[1]/width
+    phi0 = res.x[0]
+
+    if verbose:
+        print("frequency:", frequency/kHz, "kHz")
+
+    if plotting:
+        plt.plot(time/ms, phi_raw - phi0 - frequency*(time-t0), color="b", label="raw FID")
+        if smoothing:
+            plt.plot(time/ms, phi - phi0 - frequency*(time-t0), color="red", label="smoothed FID")
+        plt.plot(time[mask]/ms, phi_fit - phi0 - frequency*(time[mask]-t0), color="k", ls="--", label="fit")
+        plt.grid()
+        plt.axvspan(*(t_range/ms), color="gray", alpha=0.2)
+        plt.ylim(-0.1, 0.2)
+        plt.xlabel("time / ms")
+        plt.xlim( (np.mean(t_range)-0.55*(t_range[1]-t_range[0]))/ms, (np.mean(t_range)+0.55*(t_range[1]-t_range[0]))/ms)
+        plt.ylabel(r"$ \Phi(t) - \hat{\Phi_0} - \hat{\frac{\mathrm{d}\Phi}{\mathrm{d}t}}\cdot t$")
+        plt.legend()
+    return frequency
+
+def FID_analysis(time, flux, edge_ignore=0.1*ms, frac=0.7, probe=None, **kwargs):
+    t0 = probe.time_pretrigger
+    _, env = HilbertTransform(time, flux).EnvelopeFunction()
+    t_range = fit_range(time, env,
+                        frac=frac, edge_ignore=edge_ignore,
+                        pretrigger=probe.time_pretrigger,
+                        readout_length=probe.readout_length)
+    frequency = phase_analysis(time, flux, t0, t_range, **kwargs)
+
+    if "plotting" in kwargs.keys() and kwargs["plotting"]:
+        plt.axvline(t0/ms, ls="--", color="k")
+        plt.text(t0/ms, 0.20, "trigger", rotation=90, va="top", ha="left", fontsize=12, fontweight='bold')
+        plt.xlim(xmin=t0/ms*0.95)
+    return frequency
+
+def Echo_analysis(time, flux, t_window=0.5*ms, probe=None, **kwargs):
+    t0 = 2*probe.readout_length-probe.time_pretrigger
+    t_range = np.array([t0-t_window, t0+t_window])
+    frequency = phase_analysis(time, flux, t0, t_range, **kwargs)
+
+    if "plotting" in kwargs.keys() and kwargs["plotting"]:
+        plt.axvline(t0/ms, ls="--", color="k")
+        plt.text(t0/ms, 0.20, "Echo", rotation=90, va="top", ha="left", fontsize=12, fontweight='bold')
+
+    return frequency
