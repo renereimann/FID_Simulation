b262af7572d42f844cb18b9e6cc94879adf6b34e
Rene Reimann <rreimann@uni-mainz.de>
2021-04-22 12:35:37 +0200

    fixing min calculation

--- FreeInductionDecay/analysis/phase_fit.py
+++ FreeInductionDecay/analysis/phase_fit.py
@@ -206,7 +206,7 @@ class PhaseFitRan(object):
         self.fit_range_template = {entry["Probe ID"]: (entry["Fid Begin"], entry["Fid End"]) for entry in raw_data}
 
     def apply_smoothing(self, flux, MaxWidth=1000):
-        n = np.min(self.smoothWidth, MaxWidth)
+        n = np.min([self.smoothWidth, MaxWidth])
         smoothed_flux = flux[:]
         for iter in range(self.smooth_iterations):
             tmp = smoothed_flux[:]
