fixing nWidth calculation

FreeInductionDecay/analysis/phase_fit.py
@@ -206,13 +206,13 @@ class PhaseFitRan(object):
         self.fit_range_template = {entry["Probe ID"]: (entry["Fid Begin"], entry["Fid End"]) for entry in raw_data}
 
     def apply_smoothing(self, flux, probe_id, MaxWidth=1000):
-        n = np.min([self.smoothWidth, MaxWidth])
+        nWidth = int(np.min([self.smoothWidth, MaxWidth]))
         smoothed_flux = flux[:]
         for iter in range(self.smooth_iterations):
             tmp = smoothed_flux[:]
             for j in range(self.fit_range_template[probe_id][0], self.fit_range_template[probe_id][1]):
                 val = [smoothed_flux[j]]
-                for n in range(1, n):
+                for n in range(1, nWidth):
                     if (j >= n+ self.fit_range_template[probe_id][0]):
                         val.append(smoothed_flux[j-n])
                     if (j + n <= self.fit_range_template[probe_id][1]):