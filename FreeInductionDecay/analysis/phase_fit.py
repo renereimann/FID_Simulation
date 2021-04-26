075dc2cf5f051684d99a001a002678069a85f8a8
Rene Reimann <rreimann@uni-mainz.de>
2021-04-26 18:19:46 +0200

    some additional prints

--- FreeInductionDecay/analysis/phase_fit.py
+++ FreeInductionDecay/analysis/phase_fit.py
@@ -259,10 +259,15 @@ class PhaseFitRan(object):
         N_Eq = stop - start + 1
         MatrixData = np.array([[x[start+i]**j for j in range(NPar)] for i in range(N_Eq)])
         RHSData = np.array(y[start:])
+        print(np.shape(MatrixData), np.shape(RHSData))
         M = np.matmul(MatrixData.T,MatrixData)
+        print(np.shape(M))
         b = np.matmul(MatrixData.T,RHSData)
+        print(np.shape(b))
         M = np.linalg.inv(M)
+        print(np.shape(M))
         solution = np.matmul(M*b)
+        print(np.shape(solution))
         return solution[1,0], solution[0,0], None, None, None
 
     def fit(self, time, flux, probe_id):
